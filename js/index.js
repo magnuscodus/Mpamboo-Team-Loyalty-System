1aba079a908368585f43c186df42c903044e7734
Magnus Codus <magnuscodus@outlook.com>
2022-04-26 21:08:31 +0200

    Working on GetAccessToken Function

--- js/index.js
+++ js/index.js
@@ -1,16 +1,58 @@
+import {require} from 'requirejs';
 require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static('static'));
+/*app.use(express.static('static'));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'html/index.html'));
-});
+});*/
 
-app.get('/auth', (req, res) => {
+function GetAccessToken(){
+const data = axios({
+    method: 'GET',
+    client_id: process.env.TROVO_CLIENT_ID,
+    url: 'https://open.trovo.live/page/login.html',
+    redirect_uri: window.location.origin,
+    response_type: 'token',
+    scope: 'user_details_self',
+})
+
+/*const response = await axios({
+    url: data.url,
+    method: 'POST',
+    headers: {'content-type': 'application/json'},
+    data: JSON.stringify(data),
+})*/
+console.log(data);
+return response.data;
+}
+document.getElementById("trovoLogin").addEventListener("click", GetAccessToken); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*app.get('/auth', (req, res) => {
     res.redirect(
         `https://open.trovo.live/page/login.html?client_id=${process.env.TROVO_CLIENT_ID}&response_type=${process.env.RESPONSE_TYPE}&scope=${process.env.SCOPE}&redirect_uri=${process.env.REDIRECT_URI}`,
     );
@@ -50,4 +92,4 @@ app.get('/oauth-callback', ({
 
 app.listen(5500);
 // eslint-disable-next-line no-console
-console.log('App listening on port 5500');
+console.log('App listening on port 5500');*/
