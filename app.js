51d38ac956b2321686b6bb13b9cbdc3cc559a91b
Magnus Codus <magnuscodus@outlook.com>
2022-07-09 12:53:25 +0200

    Refactoring v1

--- app.js
+++ app.js
@@ -1,97 +1,27 @@
-require("./routes");
-const path = require("path");
-const express = require("express");
-// const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-// const exphbs = require("express-handlebars");
-const methodOverride = require("method-override");
-// const passport = require("passport");
-// const session = require("express-session");
-// const MongoStore = require("connect-mongo");
-const connectDB = require("./config/db");
-
-// * Load config
-dotenv.config({path: "./config/config.env"});
-
-// // Passport config
-// require("./config/passport")(passport);
-
-connectDB();
+const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
+const tourRouter = require('./routes/authRoutes');
+const userRouter = require('./routes/userRoutes');
 
-// // Body parser
-// app.use(express.urlencoded({extended: false}));
-// app.use(express.json());
+//*  MIDDLEWARES
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`)); //? This is how you server static files in express
 
-// Method override
-// app.use(
-// 	methodOverride(function (req, res) {
-// 		if (req.body && typeof req.body === "object" && "_method" in req.body) {
-// 			// look in urlencoded POST bodies and delete it
-// 			let method = req.body._method;
-// 			delete req.body._method;
-// 			return method;
-// 		}
-// 	})
-// );
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next(); //! Use always next, otherwise the function would get stuck without the response object
+});
 
-// Logging
-if (process.env.NODE_ENV === "development") {
-	app.use(morgan("dev"));
-}
+// * ROUTES
+//app.get('/api/v1/tours', getAllTours); //? Get all tours
+//app.get('/api/v1/tours/:id', getTour); //? Get a specific tour
+//app.post('/api/v1/tours', createTour); //? Create a tour
+//app.patch('/api/v1/tours/:id', updateTour); // ? 'patch' can be used as a http method to update data
+//app.delete('/api/v1/tours/:id', deleteTour); // ? 'deleted' can be used as a http method to delete data
+app.use('/api/v1/auth', tourRouter); // ! Mounting the tours router
+app.use('/api/v1/users', userRouter); // ! Mounting the users router
 
-// // Handlebars Helpers
-// const {formatDate, stripTags, truncate, editIcon, select} = require("./helpers/hbs");
-
-// // Handlebars
-// app.engine(
-// 	".hbs",
-// 	exphbs({
-// 		helpers: {
-// 			formatDate,
-// 			stripTags,
-// 			truncate,
-// 			editIcon,
-// 			select,
-// 		},
-// 		defaultLayout: "main",
-// 		extname: ".hbs",
-// 	})
-// );
-// app.set("view engine", ".hbs");
-
-// // Sessions
-// app.use(
-// 	session({
-// 		secret: "keyboard cat",
-// 		resave: false,
-// 		saveUninitialized: false,
-// 		store: MongoStore.create({mongoUrl: process.env.MONGO_URI}),
-// 	})
-// );
-
-// // Passport middleware
-// app.use(passport.initialize());
-// app.use(passport.session());
-
-// // Set global var
-// app.use(function (req, res, next) {
-// 	res.locals.user = req.user || null;
-// 	next();
-// });
-
-// Static folder
-app.use(express.static(path.join(__dirname, "public")));
-
-// Routes
-app.use("/", require("routes/index")); // * ΜΤΜ Homepage
-app.use("/auth", require("routes/auth")); // * Trovo Login
-
-const PORT = process.env.PORT || 3000;
-
-app.listen(
-	PORT,
-	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
-);
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB Connection successful!'));
+
+// * START SERVER
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}..`);
+});
